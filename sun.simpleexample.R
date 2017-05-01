ef6dfcc284daaeed463899f49ad5119228a41355
Monique Schafer <monique00@umail.ucsb.edu>
2017-04-30 17:33:33 -0700

    need to commit to pull files

--- sun.simpleexample.R
+++ sun.simpleexample.R
@@ -1,7 +1,14 @@
 
 sunlight = read.table("sun.txt", header=T)
-par(mar=c(5,6,3,2))
+par(mar=c(5,6,4,2))
 boxplot(sunlight$Kdown_direct~sunlight$month,
-  ylab="Downwelling Solar\n kj/m2/day",
-  xlab="Month", col="green")
+  ylab="Downwelling Solar kJ/m2/day",
+  xlab="Month", col="orange")
 
+
+climate = read.table("clim.txt", header=T)
+par(mar=c(6,6,4,2))
+boxplot(rain~month, data = climate, ylab="Precipitation", xlab="Month", col="blue")
+
+
+clim_rain <- aggregate(rain~year, data = climate, FUN = sum)
